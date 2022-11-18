fab650ad650e3de7519da9dff3a8f870b5ef30b1
hngu259 <98425842+hngu259@users.noreply.github.com>
2022-11-17 21:46:16 -0600

    Update for_pandas.py

--- for_pandas.py
+++ for_pandas.py
@@ -14,16 +14,16 @@ machines = [
 
 #2 digit int, text, text 
 muscles = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
+    [03, "Chest", "Chest"],
+    [04, "Traps", "Shoulders"],
+    [05, "Lats", "Back"], 
+    [06, "Quads", "Legs"],
+    [07, "Hamstrings", "Legs"],
+    [08, "Calves", "Legs"],
+    [09, "Abs", "Abs"],
+    [10, "Cardio", "Cardio"],
+    [11, "Forearms", "Arms"],
+    [12, "Glutes", "Legs"],
 ]
 
 #fill in
