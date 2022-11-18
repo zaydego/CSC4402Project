f3a1eb480adb7998bd088a1926c65015eddb42e9
Mbevil1 <98967196+Mbevil1@users.noreply.github.com>
2022-11-17 21:51:52 -0600

    Update for_pandas.py

--- for_pandas.py
+++ for_pandas.py
@@ -1,15 +1,15 @@
 #3 digit integer, text, text, 2 digit integer, 7 digit integer
 machines = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
+    [100, 'Seated Dips', 'Floor 1', 02, 1111111],
+    [101, 'Kettlebells', 'Floor 1', 06, 1111112],
+    [102, 'Arm Curls', 'Floor 1', 01, 1111113],
+    [103, 'Leg Press', 'Floor 1', 07, 1111114],
+    [104, 'Squats', 'Floor 1', 06, 1111115],
+    [105, 'Shoulder Press', 'Floor 1', 04, 1111116],
+    [106, 'Shoulder Raise Machine', 'Floor 2', 04, 111117],
+    [107, 'Cable Pushdowns', 'Floor 2', 02, 1111118],
+    [108, 'Military Press', 'Floor 1', 04, 1111119],
+    [109, 'Leg Extensions', 'Floor 1', 06, 1111110],
 ]
 
 #2 digit int, text, text 
