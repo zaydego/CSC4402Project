78d2196b495d6c9f5946dcfcd6d8be0a5ba80030
zaydego <60556062+zaydego@users.noreply.github.com>
2022-11-17 21:51:01 -0600

    Update for_pandas.py

--- for_pandas.py
+++ for_pandas.py
@@ -26,19 +26,19 @@ muscles = [
     [12, "Glutes", "Legs"],
 ]
 
-#fill in
+#
 users = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-]
+    [1003, 'thirduser@lsu.edu', 'Jonnie', 'Doey', 000003, 0000003],
+    [1004, 'fourthuser@lus.edu', 'Mary', 'Jane', 000004, 0000004],
+    [1005, 'fifthuser@lsu.edu', 'Jordan', 'Deef', 000005, 0000005],
+    [1006, 'sixthuser@lsu.edu', 'Jasmine', 'Mulan', 000006, 0000006],
+    [1007, 'user7@lsu.edu', 'Jake', 'Doo', 000007, 0000007],
+    [1008, 'user8@lsu.edu', 'Jill', 'Dill', 000008, 0000008],
+    [1009, 'user9@lsu.edu', 'Jorge', 'Dan', 000009, 0000009],
+    [1010, 'user10@lsu.edu', 'Jackie', 'Dope', 000010, 0000010],
+    [1011, 'user11@lsu.edu', 'James', 'Derry', 000011, 0000011],
+    [1003, 'user12@lsu.edu', 'Jucks', 'Dork', 000012, 0000012]
+        ]
 
 #fill in
 staffs = [
